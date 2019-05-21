5c0dbb77fe1f27c852763137f54cd4e261300f4d
Jiajun Chen <jiajun.chen@nuance.com>
2019-05-21 13:31:40 -0400

    reformate the code

--- java/q001_to_q100/Num_2_AddTwoNumbers.java
+++ java/q001_to_q100/Num_2_AddTwoNumbers.java
@@ -3,39 +3,40 @@ package q001_to_q100;
 import utils.ListNode;
 
 public class Num_2_AddTwoNumbers {
-    public void main(String[] args) {
 
+  public void main(String[] args) {
+
+  }
+
+  public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode ptr1 = l1;
+    ListNode ptr2 = l2;
+    ListNode solution = new ListNode((ptr1.val + ptr2.val) % 10);
+    ListNode ptrSolution = solution;
+    int plusOne = (ptr1.val + ptr2.val) / 10;
+    while (ptr1.next != null && ptr2.next != null) {
+      ptr1 = ptr1.next;
+      ptr2 = ptr2.next;
+      ptrSolution.next = new ListNode((ptr1.val + ptr2.val + plusOne) % 10);
+      plusOne = (ptr1.val + ptr2.val + plusOne) / 10;
+      ptrSolution = ptrSolution.next;
+    }
+    while (ptr1.next != null) {
+      ptr1 = ptr1.next;
+      ptrSolution.next = new ListNode((ptr1.val + plusOne) % 10);
+      plusOne = (ptr1.val + plusOne) / 10;
+      ptrSolution = ptrSolution.next;
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ptr1 = l1;
-        ListNode ptr2 = l2;
-        ListNode solution = new ListNode((ptr1.val + ptr2.val) % 10) ;
-        ListNode ptrSolution = solution;
-        int plusOne = (ptr1.val + ptr2.val) / 10;
-        while(ptr1.next != null && ptr2.next != null) {
-            ptr1 = ptr1.next;
-            ptr2 = ptr2.next;
-            ptrSolution.next = new ListNode((ptr1.val + ptr2.val + plusOne) % 10);
-            plusOne = (ptr1.val + ptr2.val + plusOne) / 10;
-            ptrSolution = ptrSolution.next;
-        }
-        while(ptr1.next != null) {
-            ptr1 = ptr1.next;
-            ptrSolution.next = new ListNode((ptr1.val + plusOne) % 10);
-            plusOne = (ptr1.val + plusOne) / 10;
-            ptrSolution = ptrSolution.next;
-        }
-
-        while(ptr2.next != null) {
-            ptr2 = ptr2.next;
-            ptrSolution.next = new ListNode((ptr2.val + plusOne) % 10);
-            plusOne = (ptr2.val + plusOne) / 10;
-            ptrSolution = ptrSolution.next;
-        }
-        if (plusOne != 0) {
-            ptrSolution.next = new ListNode(1);
-        }
-        return solution;
+    while (ptr2.next != null) {
+      ptr2 = ptr2.next;
+      ptrSolution.next = new ListNode((ptr2.val + plusOne) % 10);
+      plusOne = (ptr2.val + plusOne) / 10;
+      ptrSolution = ptrSolution.next;
     }
+    if (plusOne != 0) {
+      ptrSolution.next = new ListNode(1);
+    }
+    return solution;
+  }
 }
