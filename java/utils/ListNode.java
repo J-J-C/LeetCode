75de1dc6222d8c6e2512cf670629b797ea1ad336
Jiajun Chen <jiajun.chen@nuance.com>
2019-06-17 16:02:03 -0400

    solved 21 and 7

--- java/utils/ListNode.java
+++ java/utils/ListNode.java
@@ -7,4 +7,18 @@ public class ListNode {
   public ListNode(int val) {
     this.val = val;
   }
+
+
+  public static void printList(ListNode node) {
+    StringBuilder builer = new StringBuilder();
+    ListNode pointer = node;
+    while(pointer != null) {
+      builer.append(pointer.val);
+      if (pointer.next != null) {
+        builer.append("->");
+      }
+      pointer = pointer.next;
+    }
+    System.out.println(builer.toString());
+  }
 }
