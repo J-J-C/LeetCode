fcefdc4cab8858f83246b74def0a95f124763451
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2019-07-28 19:46:14 -0400

    solve num 236

---
+++ java/q201_q300/Num_236_LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,33 @@
+package q201_q300;
+
+import utils.TreeNode;
+
+public class Num_236_LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null) {
+            return null;
+        }
+        if (root.left == p || root.right == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // if both exists, this means root is the ancestor
+        if (left != null && right != null) {
+            return root;
+        }
+        // we found the ancestor on the left side
+        if (left != null) {
+            return left;
+        }
+        // we found on the right side
+        if (right != null) {
+            return right;
+        }
+        // DNE
+        return null;
+    }
+}
