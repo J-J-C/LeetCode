f27b43f98b6e549b3a851ba23f11fe238be20efd
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-01-08 23:19:19 -0500

    solved question 1249

--- java/q1000_and_after/Num1249_MinimumRemoveToMakeValidParentheses.java
+++ java/q1000_and_after/Num1249_MinimumRemoveToMakeValidParentheses.java
@@ -1,84 +1,118 @@
 package q1000_and_after;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.Stack;
 
 public class Num1249_MinimumRemoveToMakeValidParentheses {
-    public static void main(String[] args) {
-        String test = "lee(t(c)o)de)";
-        System.out.println(minRemoveToMakeValidString(test));
 
-//        test = "a)b(c)d";
-//        System.out.println(minRemoveToMakeValidString(test));
-//
-//        test = "))((";
-//        System.out.println(minRemoveToMakeValidString(test));
-//
-//        test = "(a(b(c)d)";
-//        System.out.println(minRemoveToMakeValidString(test));
+
+    @Test
+    public void testMethod() {
+
+        Assert.assertTrue("lee(t(co)de)".equals(minRemoveToMakeValid("lee(t(c)o)de)")) || "lee(t(c)o)de".equals(minRemoveToMakeValid("lee(t(c)o)de)")));
+        Assert.assertEquals("ab(c)d", minRemoveToMakeValid("a)b(c)d"));
+        Assert.assertEquals("", minRemoveToMakeValid("))(("));
+        Assert.assertEquals("(a(bc)d)", minRemoveToMakeValid("(a(b(c)d)"));
+        Assert.assertEquals("", minRemoveToMakeValid("))))"));
+
+        Assert.assertEquals("", minRemoveToMakeValid(")))))"));
+        Assert.assertEquals("()()", minRemoveToMakeValid("())()((("));
+        Assert.assertEquals("i()()(fm)", minRemoveToMakeValid(")i()()((fm(((()"));
+        Assert.assertEquals("(t(e)y)d()e", minRemoveToMakeValid("(t(e)y))d(()(e("));
     }
 
-    public static String minRemoveToMakeValidString(String string) {
+
+    public static String minRemoveToMakeValid(String string) {
         // assume string is all lower case
         if (string.length() == 0) {
             return string;
         }
-        if(!string.contains("(") || !string.contains(")")) {
+        if (!string.contains("(") && !string.contains(")")) {
             return string;
         }
-        StringBuilder frontBuilder = new StringBuilder();
-        StringBuilder backBuilder = new StringBuilder();
-        int head = 0;
-        int tail = string.length() - 1;
-        Stack s = new Stack();
-        while(head < tail) {
-            char headChar = string.charAt(head);
-            char tailChar = string.charAt(tail);
 
-            while(isLowLetter(headChar) || headChar == ')') {
-                if(headChar == ')') {
-                    // do nothing
-                } else {
-                    frontBuilder.append(headChar);
-                }
-                head++;
-                headChar = string.charAt(head);
-            }
-            System.out.println("stopped");
-            if(headChar == '(') {
-                while(isLowLetter(tailChar) || tailChar == '(') {
-                    if(headChar == '(') {
-                        // do nothing
-                    } else {
-                        backBuilder.append(tailChar);
-                    }
-                    tail--;
-                    tailChar = string.charAt(tail);
-                }
-                if(tailChar == ')') {
-                    if(head < tail) {
-                        frontBuilder.append(headChar);
-                        backBuilder.append(tailChar);
-                        head++;
-                        tail--;
-                    } else {
-                        break;
-                    }
-                }
-            }
+        // using a stack to memorize parentheses
+        Stack stack = new Stack<>();
 
-            System.out.println(frontBuilder.toString());
-            System.out.println(backBuilder.reverse().toString());
-            System.out.println(head);
-            System.out.println(tail);
+        char[] array = string.toCharArray();
+        StringBuilder builder = new StringBuilder();
+
+        // scanning forward....
+        for (int i = 0; i < array.length; i++) {
+            char c = string.charAt(i);
+            if (string.charAt(i) == '(') {
+                stack.push('(');
+                builder.append(c);
+            }
+            else if (c == ')') {
+                if (!stack.empty()) {
+                    stack.pop();
+                    builder.append(c);
+                }
+            } else {
+                builder.append(c);
+            }
         }
-        return frontBuilder.toString() + backBuilder.reverse().toString();
+        // scanning backward
+        while(!stack.isEmpty()) {
+            builder.deleteCharAt(builder.lastIndexOf("("));
+            stack.pop();
+        }
+        return builder.toString();
+    }
+
+
+    public static String minRemoveToMakeValidOriginal(String string) {
+        // assume string is all lower case
+        if (string.length() == 0) {
+            return string;
+        }
+        if (!string.contains("(") && !string.contains(")")) {
+            return string;
+        }
+
+        // using a stack to memorize parentheses
+        Stack stack = new Stack<>();
+
+        char[] array = string.toCharArray();
+
+
+        // scanning forward....
+        for(int i = 0; i < array.length; i++) {
+            if(isLowLetter(array[i])) {
+                continue;
+            }
+            if(array[i] == '(') {
+                stack.push('(');
+                continue;
+            }
+            if(array[i] == ')') {
+                if(!stack.empty()) {
+                    stack.pop();
+                    continue;
+                } else{
+                    array[i] = '1';
+                }
+            }
+        }
+        // scanning backward
+        for(int i = array.length - 1; i >=0; i--) {
+            if(array[i] == '(') {
+                if(!stack.isEmpty()) {
+                    array[i] = '1';
+                    stack.pop();
+                }
+            }
+        }
+        return new String(array).replaceAll("1", "");
+
     }
 
 
     private static boolean isLowLetter(char c) {
-//        System.out.println(c);
-//        System.out.println(c >= 97 && c <=122);
-        return c >= 97 && c <=122;
+        return c >= 97 && c <= 122;
     }
 
 }
