a1e92d5771c5cb633b1e9ee64cff950db1577ebf
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-03-30 22:28:04 -0400

    solve 543

--- java/q401_to_q500/Num437_PathSum3.java
+++ java/q401_to_q500/Num437_PathSum3.java
@@ -6,33 +6,17 @@ public class Num437_PathSum3 {
 
 
     public int pathSum(TreeNode root, int sum) {
-
         if (root == null) {
             return 0;
         }
-
-
-
-        if (root.val == sum) {
-            return 1 + pathSum(root.left, sum) + pathSum(root.right, sum) + pathSumFrom(root.left, sum - root.val) + pathSumFrom(root.right, sum - root.val);
-        } else {
-            return 0 + pathSum(root.left, sum) + pathSum(root.right, sum) + pathSumFrom(root.left, sum - root.val) + pathSumFrom(root.right, sum - root.val);
-        }
-
-
+        return pathSum(root.left, sum) + pathSum(root.right, sum) + pathSumFrom(root, sum);
     }
 
     private int pathSumFrom(TreeNode root, int sum) {
-        if(root == null) {
+        if (root == null) {
             return 0;
         }
-        if(root.left == null && root.right == null) {
-            if(root.val == sum) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        return pathSumFrom(root.right, sum - root.val) + pathSumFrom(root.left, sum - root.val);
+        return (root.val == sum ? 1 : 0) + pathSumFrom(root.right, sum - root.val) + pathSumFrom(root.left, sum - root.val);
+
     }
 }
