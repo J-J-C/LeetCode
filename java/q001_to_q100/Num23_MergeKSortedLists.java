cc962c9c0a28000bc2a032c9ddb429421566b70d
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-04-10 16:17:23 -0400

    rework 23

--- java/q001_to_q100/Num23_MergeKSortedLists.java
+++ java/q001_to_q100/Num23_MergeKSortedLists.java
@@ -1,54 +1,50 @@
 package q001_to_q100;
 
+import utils.ListNode;
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
-
-import utils.ListNode;
 
 
 public class Num23_MergeKSortedLists {
 
     public ListNode mergeKLists(ListNode[] lists) {
         // two base case
-        if(lists.length == 0) {
+        if (lists.length == 0) {
             return null;
         }
-        if(lists.length == 1) {
+        if (lists.length == 1) {
             return lists[0];
         }
 
-        Comparator<ListNode> listNodeComparator = new Comparator<ListNode>() {
+        PriorityQueue<ListNode> queue = new PriorityQueue(new Comparator<ListNode>() {
             @Override
             public int compare(ListNode o1, ListNode o2) {
                 return o1.val - o2.val;
             }
-        };
-        PriorityQueue<ListNode> queue = new PriorityQueue<>(listNodeComparator);
-        for(ListNode node : lists) {
-            if(node != null) {
-                queue.add(node);
-            }
+        });
+
+        ListNode head = new ListNode();
+        ListNode pointer = head;
+        ListNode previous = pointer;
+        for (ListNode node : lists) {
+            if (node != null) queue.add(node);
         }
-        if(queue.isEmpty()) {
+        if (queue.isEmpty()) {
             return null;
         }
-        ListNode head = null;
-        ListNode pointer = head;
 
-        ListNode firstNode = queue.poll();
-        head = firstNode;
-        pointer = head;
-        if(firstNode.next != null) {
-            queue.add(firstNode.next);
-        }
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             ListNode node = queue.poll();
-            pointer.next = new ListNode(node.val);
+            pointer.val = node.val;
+            pointer.next = new ListNode();
+            previous = pointer;
             pointer = pointer.next;
-            if(node.next != null) {
+            if (node.next != null) {
                 queue.add(node.next);
             }
         }
+        previous.next = null;
         return head;
     }
 }
