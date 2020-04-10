0e55073c9967a54d03eba8188dac72f77aafb5de
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-04-10 19:10:47 -0400

    .gitignore is now working

---
+++ java/src/utils/ListNode.java
@@ -0,0 +1,27 @@
+package utils;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+
+    }
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+
+    public static void printList(ListNode node) {
+        StringBuilder builer = new StringBuilder();
+        ListNode pointer = node;
+        while (pointer != null) {
+            builer.append(pointer.val);
+            if (pointer.next != null) {
+                builer.append("->");
+            }
+            pointer = pointer.next;
+        }
+        System.out.println(builer.toString());
+    }
+}
