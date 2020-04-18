4bc1e30cb955ee0e31197000662b0ef0f66a13a1
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-04-18 14:55:35 -0400

    solved 1134, 1323 151

---
+++ java/src/math/Num1134_ArmstrongNumber.java
@@ -0,0 +1,25 @@
+package src.math;
+
+public class Num1134_ArmstrongNumber {
+
+    public boolean isArmstrong(int n) {
+
+        int counter = 0;
+        int number = n;
+        int finalNumber = n;
+
+
+        while (number > 0) {
+            number /= 10;
+            counter++;
+        }
+        int sum = 0;
+
+        while (n > 0) {
+            sum += (int) Math.pow(n % 10, counter);
+            n /= 10;
+        }
+
+        return finalNumber == sum;
+    }
+}
---
+++ java/src/math/Num1323_Maximum69Number.java
@@ -0,0 +1,32 @@
+package src.math;
+
+public class Num1323_Maximum69Number {
+
+    public int maximum69Number(int num) {
+
+        int[] digits = Integer.toString(num).chars().map(c -> c - '0').toArray();
+        boolean changed = false;
+        int sum = 0;
+        for (int i = 0; i < digits.length; i++) {
+            sum *= 10;
+            if (digits[i] != 9 && !changed) {
+                digits[i] = 9;
+                changed = true;
+            }
+            sum += digits[i];
+        }
+        return sum;
+    }
+
+    public int maximum69NumberOptimal(int num) {
+        int firstSix = -1;
+        int number = num;
+        for (int i = 0; number > 0; i++) {
+            if (number % 10 == 6) {
+                firstSix = i;
+            }
+            number /= 10;
+        }
+        return num + 3 * (int) Math.pow(10, firstSix);
+    }
+}
