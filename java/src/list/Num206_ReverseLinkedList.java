c2a1e8fa0b43491730ff1c154d689882c39e5f01
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-04-30 20:45:57 -0400

    4/29 SOLVED 206 387

---
+++ java/src/list/Num206_ReverseLinkedList.java
@@ -0,0 +1,60 @@
+package list;
+
+import utils.ListNode;
+
+public class Num206_ReverseLinkedList {
+    ListNode newHead = null;
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public ListNode reverseListR(ListNode head) {
+        /* recursive solution */
+        return reverseListInt(head, null);
+    }
+
+    private ListNode reverseListInt(ListNode head, ListNode newHead) {
+        if (head == null)
+            return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListInt(next, head);
+    }
+
+    public ListNode reverseListR2(ListNode head) {
+        // base case
+        if (head == null || head.next == null) return head;
+
+        ListNode newHead = reverseListR2(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public ListNode reverseList3(ListNode head) {
+        helper(head);
+        return newHead;
+    }
+
+    private ListNode helper(ListNode node) {
+        if (node == null || node.next == null) {
+            newHead = node;
+            return node;
+        }
+        ListNode temp = helper(node.next);
+        temp.next = node;
+        node.next = null;
+        return node;
+    }
+}
---
+++ java/src/string/Num387_FirstUniqueCharacterInAString.java
@@ -0,0 +1,54 @@
+package string;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Num387_FirstUniqueCharacterInAString {
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqCharArray("leetcode"));
+    }
+
+    public static int firstUniqCharArray(String s) {
+
+        int[] map = new int[26];
+        Arrays.fill(map, -1);
+        char base = 'a';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map[c - base] == -1) {
+                map[c - base] = i;
+            } else {
+                map[c - base] = Integer.MAX_VALUE;
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i : map) {
+            if (i < min && i != -1) {
+                min = i;
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, i);
+            } else {
+                map.put(c, Integer.MAX_VALUE);
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i : map.values()) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
