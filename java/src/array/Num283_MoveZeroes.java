18868d730b0c0a02ae22ce836b83ba28c94a1d94
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-05-02 19:16:27 -0400

    4/30 solved 226, 104, 115, 283

---
+++ java/src/array/Num283_MoveZeroes.java
@@ -0,0 +1,32 @@
+package src.array;
+
+import java.util.Arrays;
+
+public class Num283_MoveZeroes {
+
+    public static void main(String[] args) {
+        int[] array = {1, 0, 3, 12};
+        moveZeroes(array);
+
+        int[] array2 = {0, 1, 0, 3, 12};
+        moveZeroes(array2);
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int pointer = 0;
+        int zeroIndex = 0;
+        while (pointer < nums.length) {
+            if (nums[pointer] != 0) {
+                int temp = nums[pointer];
+                nums[pointer] = nums[zeroIndex];
+                nums[zeroIndex] = temp;
+
+                pointer++;
+                zeroIndex++;
+            } else {
+                pointer++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
