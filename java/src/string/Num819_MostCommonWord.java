b98d083540ece022ac4fbba357c594909e4792fe
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-05-05 23:20:10 -0400

    5/5 solved 704, 278, 139, 819

---
+++ java/src/string/Num819_MostCommonWord.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.*;
+
+public class Num819_MostCommonWord {
+
+    public static void main(String[] args) {
+        String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        String[] banned = {"hit"};
+        mostCommonWord(paragraph, banned);
+    }
+
+    public static String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> bannedSet = new HashSet<>(Arrays.asList(banned));
+        String[] words = paragraph.split("([^a-zA-Z]+)");
+        System.out.println(Arrays.toString(words));
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (!bannedSet.contains(word)) {
+                map.put(word.toLowerCase(), map.getOrDefault(word.toLowerCase(), 0) + 1);
+            }
+        }
+        return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
