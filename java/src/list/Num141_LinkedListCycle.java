1e5cadaa1d0de0d80dde7e794c856e6ae8093391
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-05-10 23:18:55 -0400

    5/10 solved 141, 67, 76

---
+++ java/src/list/Num141_LinkedListCycle.java
@@ -0,0 +1,25 @@
+package list;
+
+import utils.ListNode;
+
+public class Num141_LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode doublePtr = head;
+        ListNode ptr = head;
+        while (doublePtr != null) {
+            ptr = ptr.next;
+            doublePtr = doublePtr.next;
+            if (doublePtr != null) {
+                doublePtr = doublePtr.next;
+            }
+            if (ptr == doublePtr) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ java/src/math/Num67_AddBinary.java
@@ -0,0 +1,55 @@
+package math;
+
+public class Num67_AddBinary {
+
+    public static void main(String[] args) {
+        System.out.println(addBinary("1", "111"));
+    }
+
+    public static String addBinary(String a, String b) {
+        StringBuilder builder = new StringBuilder();
+        boolean carryOver = false;
+        if (a.length() < b.length()) {
+            String temp = a;
+            a = b;
+            b = temp;
+        }
+        int ap = a.length() - 1;
+        int bp = b.length() - 1;
+        while (ap >= 0 && bp >= 0) {
+            if (a.charAt(ap) == b.charAt(bp)) {
+                if (carryOver) {
+                    builder.append('1');
+                } else {
+                    builder.append('0');
+                }
+                carryOver = a.charAt(ap) == '1';
+            } else {
+                if (carryOver) {
+                    builder.append('0');
+                } else {
+                    builder.append('1');
+                }
+            }
+            ap--;
+            bp--;
+        }
+        while (ap >= 0) {
+            if (carryOver) {
+                if (a.charAt(ap) == '0') {
+                    carryOver = false;
+                    builder.append('1');
+                } else {
+                    builder.append('0');
+                }
+            } else {
+                builder.append(a.charAt(ap));
+            }
+            ap--;
+        }
+        if (carryOver) {
+            builder.append('1');
+        }
+        return builder.reverse().toString();
+    }
+}
