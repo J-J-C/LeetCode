37c922fce3016df5080a66345b84f3f75aeda109
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-05-12 21:29:49 -0400

    5/12 solved 169, 448, will begin dynamic programming soon

---
+++ java/src/array/Num169_MajorityElement.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Num169_MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int count = 0, result = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                result = num;
+            }
+            if (num != result) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+        return result;
+    }
+}
---
+++ java/src/array/Num448_FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Num448_FindAllNumbersDisappearedInAnArray {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        int[] memory = new int[nums.length + 1];
+        for (int i : nums) {
+            memory[i]++;
+        }
+        for (int i = 1; i < memory.length; i++) {
+            if (memory[i] == 0) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
