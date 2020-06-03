b55d62edd8e2b8ec63081fd87ea12a6f42034413
Jiajun Chen <jiajun.chen@mail.mcgill.ca>
2020-06-02 22:08:31 -0400

    5/31 solved 11 and 23

---
+++ java/src/list/Num23_MegeKSortedLists.java
@@ -0,0 +1,29 @@
+package list;
+
+import utils.ListNode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Num23_MegeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode result = new ListNode();
+        ListNode pointer = result;
+
+        PriorityQueue<ListNode> nodes = new PriorityQueue<>((Comparator<ListNode>) (o1, o2) -> o1.val - o2.val);
+        for (ListNode node : lists) {
+            if (node != null) {
+                nodes.add(node);
+            }
+        }
+        while (!nodes.isEmpty()) {
+            ListNode node = nodes.poll();
+            pointer.next = new ListNode(node.val);
+            pointer = pointer.next;
+            if (node.next != null) {
+                nodes.add(node.next);
+            }
+        }
+        return result.next;
+    }
+}
